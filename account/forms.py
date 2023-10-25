6259a44eb1d1b851cb14abf21dd949830e636e14
yusuffinan <yusufserhaat@gmail.com>
2023-10-25 17:08:00 +0300

    AuthenticationForm

---
+++ account/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.forms import widgets
+from django.contrib.auth.models import User
+
+class LoginUserForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget = widgets.TextInput(attrs={"class":"form-control"})
+        self.fields["password"].widget = widgets.PasswordInput(attrs={"class":"form-control"}) 
+
+    def confirm_login_allowed(self, user):
+        if user.username.startswith("si"):
+            raise forms.ValidationError("bu kullanıcı adı kabul edilmemektedir")
+class NewUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username","email","first_name","last_name")
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget = widgets.TextInput(attrs={"class":"form-control"})
+        self.fields["password1"].widget = widgets.PasswordInput(attrs={"class":"form-control"})
+        self.fields["password2"].widget = widgets.PasswordInput(attrs={"class":"form-control"})
+        self.fields["email"].widget = widgets.EmailInput(attrs={"class":"form-control"})
+        self.fields["first_name"].widget = widgets.TextInput(attrs={"class":"form-control"})
+        self.fields["last_name"].widget = widgets.TextInput(attrs={"class":"form-control"})
+
+    
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+
+        if User.objects.filter(email=email).exists():
+            self.add_error("email","bu email daha önce alınmış")
+
+        return email
+
+
--- account/views.py
+++ account/views.py
@@ -1,60 +1,54 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from account.forms import LoginUserForm, NewUserForm
 
 def user_login(request):
     if request.user.is_authenticated and "next" in request.GET:
            return render(request, "account/login.html",{"error":"Yetkiniz yok"})
+    
     if request.method=="POST":
-        username=request.POST["username"]
-        password=request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            next_url = request.GET.get("next", None)
-            if next_url is None:
-                return redirect("index")
+        form = LoginUserForm(request, data=request.POST)
+        if form.is_valid():
+            username=form.cleaned_data.get("username")
+            password=form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.add_message(request,messages.SUCCESS, "Giriş başarılı.")
+                next_url = request.GET.get("next", None)
+                if next_url is None:
+                    return redirect("index")
+                else:
+                    return redirect(next_url)
             else:
-                return redirect(next_url)
+                return render(request, "account/login.html", {"form":form}) 
             
         else:
-            return render(request, "account/login.html",{"error":"Hatalı bili girdiniz"})
+            return render(request, "account/login.html", {"form":form})
     else:
-        return render(request, "account/login.html")
+        form = LoginUserForm()
+        return render(request, "account/login.html", {"form": form})
 
 def user_register(request):
     if request.method=="POST":
-        username=request.POST["username"]
-        email=request.POST["email"]
-        password=request.POST["password"]
-        repassword=request.POST["repassword"]
+       form = NewUserForm(request.POST)
 
-        if repassword != password:
-            return render(request,"account/register.html",{
-                "error":"Şifreler uyuşmuyor.",
-                "username": username,
-                "email":email
-            })
-        if User.objects.filter(username=username).exists():
-            return render(request,"account/register.html",{
-                "error":"Bu kullanıcı adı zaten alınmış.",
-                "username": username,
-                "email":email
-                })
-        if User.objects.filter(email=email).exists():
-            return render(request,"account/register.html",{
-                "error":"Bu email zaten alınmış.",
-                "username": username,
-                "email":email
-                
-                })
-        
-        user = User.objects.create_user(username=username, password=password, email= email)
-        user.save()
-        return redirect("user_login")
+       if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return redirect("index")
+       else:
+           return render(request, "account/register.html", {"form":form})
     else:
-        return render(request,"account/register.html")
+        form = NewUserForm()
+        return render(request, "account/register.html", {"form":form})
 def user_logout(requst):
     logout(requst)
+    messages.add_message(requst,messages.ERROR,"Çıkış yapıldı")
     return redirect("user_login")
