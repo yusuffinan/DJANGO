791bbfa8305e0e5e75312cece29e90955bd4efa4
yusuffinan <yusufserhaat@gmail.com>
2023-10-26 13:17:26 +0300

    password

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
 from django.forms import widgets
 from django.contrib.auth.models import User
 
@@ -35,3 +35,10 @@ class NewUserForm(UserCreationForm):
         return email
 
 
+class UserPasswordChangeForm(PasswordChangeForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["old_password"].widget = widgets.PasswordInput(attrs={"class":"form-control"})
+        self.fields["new_password1"].widget = widgets.PasswordInput(attrs={"class":"form-control"})
+        self.fields["new_password2"].widget = widgets.PasswordInput(attrs={"class":"form-control"})
--- account/views.py
+++ account/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
-from account.forms import LoginUserForm, NewUserForm
-
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from account.forms import LoginUserForm, NewUserForm, UserPasswordChangeForm
+    
 def user_login(request):
     if request.user.is_authenticated and "next" in request.GET:
            return render(request, "account/login.html",{"error":"Yetkiniz yok"})
@@ -52,3 +52,16 @@ def user_logout(requst):
     logout(requst)
     messages.add_message(requst,messages.ERROR,"Çıkış yapıldı")
     return redirect("user_login")
+
+def change_password(request):
+    if request.method == "POST":
+        form = UserPasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, "Parola Güncellendi")
+            return redirect("change_password")
+        else:
+            return render(request, "account/change-password.html", {"form": form})
+    form = UserPasswordChangeForm(request.user)
+    return render(request, "account/change-password.html", {"form": form})
