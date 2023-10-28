fbfe1d6f88b7450d9fa3912fe993d7a588c3b411
yusuffinan <yusufserhaat@gmail.com>
2023-10-28 23:57:24 +0300

    back

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -22,14 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-uw)7b2o(psi)el%sbcy$7r3byss+$tn0aazd+i+n!$cyd5lenf'
-SECRET_KEY = getenv("SECRET_KEY")
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getenv("IS_DEVELOPMENT", True)
+SECRET_KEY = 'django-insecure-uw)7b2o(psi)el%sbcy$7r3byss+$tn0aazd+i+n!$cyd5lenf'
 
-ALLOWED_HOSTS = [
-    getenv("APP_HOST")
-]
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 
 # Application definition
